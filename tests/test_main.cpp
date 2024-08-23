64e8ef6c00085868937077eb325e6dfc3a2e0452
Yijun Tang <yijuntang94@gmail.com>
2024-08-23 22:14:13 +0800

    project setup

---
+++ tests/test_main.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+// A simple test case
+TEST(HelloTest, BasicAssertions) {
+    EXPECT_STRNE("hello", "world");
+    EXPECT_EQ(7 * 6, 42);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
